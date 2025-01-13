3aa0c29b3ebaa2c260acde3c280b8872968abf72
vagg <vagg@visavis>
2025-01-13 14:30:34 +0200

    feat : added install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git clone https://github.com/asrevni/qbitban
+cd qbitban
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install pyinstaller
+pyinstaller --name qbitban qbitban.py
+chmod +x qbitban
+chmod +x dist/qbitban/qbitban
+mv -r dist/qbitban /usr/local/bin/
+mv qbitban.json /usr/local/etc/
+mv qbitban /usr/local/etc/rc.d/
+cd .. && rm -r qbitban
+
+echo "To enable run 'sysrc qbitban_enable=YES' and 'service qbitban start'"
+echo "Add details to configuration file in '/usr/local/etc/qbitban.json'"
